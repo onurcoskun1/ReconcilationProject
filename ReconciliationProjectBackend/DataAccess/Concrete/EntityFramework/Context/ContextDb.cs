c3ca6859a6a83f5d3153bab81fc8fd807ca1a1ab
onurcoskun1 <coskunonur01@gmail.com>
2022-05-03 22:45:10 +0300

    Veri tabanı tasarlandı. Katmanlar ve sınıflar oluşturuldu.

---
+++ ReconciliationProjectBackend/DataAccess/Concrete/EntityFramework/Context/ContextDb.cs
@@ -0,0 +1,33 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=POSEIDON\MSSQLSERVER1; Database=ReconcilationDb;IntegratedSecurity=true");
+        }
+
+        public DbSet<AccountReconciliation> AccountReconciliations { get; set; }
+        public DbSet<AccountReconciliationDetail> AccountReconciliationDetail { get; set; }
+        public DbSet<BaBsReconciliation> BaBsReconciliations { get; set; }
+        public DbSet<BaBsReconciliationDetail> BaBsReconciliationDetails { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<CurrencyAccount> CurrencyAccounts { get; set; }
+        public DbSet<EmailParameter> EmailParameters { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserCompany> UserCompanies { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+
+    }
+}
